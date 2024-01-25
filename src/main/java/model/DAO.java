ac7231db4f412d247148a0d987a72aefaeee0957
Adriel Vinicius <adriel1234.1234@gmail.com>
2024-01-24 21:41:42 -0400

    conexao banco de dados

--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -1,5 +1,51 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.servlet.jsp.el.ELParseException;
+
 public class DAO {
+	
+	/** Modulo conexão **/
+	
+	//parametros de conexao
+	
+	private String driver="com.mysql.jdbc.Driver";
+	private String url="jdbc:mysql://localhost:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
+	private String user ="root";
+	private String password="root";
+	
+	// Método url
+	
+	private Connection conectar() {
+		Connection con = null;
+		
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			
+			return con;
+		}catch (Exception e) {
+			System.err.println(e.getMessage());
+			
+			return null;
+		}
+	}
+	
+	//teste conexao
+	
+//	public void testeConexa() {
+//		try {
+//			Connection con = conectar();
+//			System.out.println(con);
+//			con.close();
+//		}catch (Exception e) {
+//			System.err.println(e.getMessage());
+//		}
+//		
+//	}
+	
 
 }
