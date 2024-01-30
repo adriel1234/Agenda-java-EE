60bd6c322b701c9f5a071b431c89f2a08bc5a275
Adriel Vinicius <adriel1234.1234@gmail.com>
2024-01-30 15:21:28 -0400

    metodo inserir ok

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -7,11 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.DAO;
+import model.JavaBeans;
 
-@WebServlet(urlPatterns = { "/Controller", "/main" })
+@WebServlet(urlPatterns = { "/Controller", "/main","/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
+	JavaBeans contato = new JavaBeans();
 
 	public Controller() {
 		super();
@@ -25,6 +27,11 @@ public class Controller extends HttpServlet {
 		if (action.equals("/main")) {
 			contatos(request, response);
 
+		}else if(action.equals("/insert")) {
+			novoContato(request,response);
+			
+		}else {
+			response.sendRedirect("index.html");
 		}
 		// teste conexao
 //		
@@ -35,5 +42,26 @@ public class Controller extends HttpServlet {
 	protected void contatos(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.sendRedirect("agenda.jsp");
+	}
+	
+	//Novov contato
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//teste de recebimento dos formulario
+//		System.out.println(request.getParameter("nome"));
+//		System.out.println(request.getParameter("fone"));
+//		System.out.println(request.getParameter("email"));
+		
+		// setar as variáveis JavaBeans
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+		
+		//Invocar  o método inserirContatos passando o objeto contato
+		dao.inserirContato(contato);
+		
+		// redirencionar para o documento agenda.jsp
+		response.sendRedirect("main");
+		
 	}
 }
--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -3,6 +3,8 @@ package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import com.mysql.jdbc.PreparedStatement;
+
 public class DAO {
 	
 	/** Modulo conexão **/
@@ -30,6 +32,39 @@ public class DAO {
 			return null;
 		}
 	}
+	
+	/** CRUD CREATE**/
+	
+	public void inserirContato(JavaBeans contato) {
+		String create = "insert into contatos (nome,fone,email) values(?,?,?)";
+		
+		try {
+			//abrir conexao banco
+			
+			Connection con = conectar();
+			
+			//Preparara a query para ser executada no banco de dados
+			PreparedStatement pst = (PreparedStatement) con.prepareStatement(create);
+			
+			// Substituir os parâmetros (?) pelo conteúdo das variáveis JavaBeans
+			pst.setString(1, contato.getNome());
+			pst.setString(2, contato.getFone());
+			pst.setString(3, contato.getEmail());
+			
+			
+			//executar a query
+			pst.executeUpdate();
+			
+			
+			//encerrar a conexão com o banco
+			con.close();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+	}
+	
 	
 	//teste conexao
 	
